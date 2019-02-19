f03b2c6482097e931939788f27b3aa08e839352b
funknug <47706680+funknug@users.noreply.github.com>
2019-02-19 14:22:38 -0600

    updated email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -15,8 +15,8 @@ $to = "funknugav@gmail.com"; // Add your email address inbetween the "" replacin
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
 $header = "From: noreply@funknug.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$header .= "Reply-To: funknugav@gmail.com";
-
+$header .= "Reply-To: $email";
+funknugav@gmail.com
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
 ?>
